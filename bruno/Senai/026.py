e9eacb49f19f9878d47a2f2a5370a31029799dd6
Bruno Campioni <133364100+Campionii@users.noreply.github.com>
2024-07-30 18:26:18 -0300

    Update 026.py

--- bruno/Senai/026.py
+++ bruno/Senai/026.py
@@ -4,8 +4,26 @@
 # abaixo do IMC ideal.
 
 
-peso = float(input('Qual o seu peso? '))
-altura = float(input('Qual s sua altura? '))
+#Abaixo de 18,5: Abaixo do peso
+#18,5 a 24,9: Peso normal
+#25 a 29,9: Sobrepeso
+#30 ou mais: Obesidade
 
-IMC = peso / (altura ** 2)
+peso = float(input('Qual o seu peso(Kg): '))
+
+altura = float(input('Qual a sua altura(m): '))
+
+imc = peso / (altura ** 2)
+
+if imc >= 30: 
+    print(f'IMC: {imc:.2f} "Obesidade"')
+
+elif imc >= 25: 
+    print(f'IMC: {imc:.2f} "Sobrepeso"')
+
+elif imc >= 18.5: 
+    print(f'IMC: {imc:.2f} "Peso normal"')
+
+else:
+    print(f'IMC: {imc:.2f} "Abaixo do peso"')
 
