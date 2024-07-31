a673e3c01a2781b402d17fc712cebcc9b80238ff
Bruno Campioni <133364100+Campionii@users.noreply.github.com>
2024-07-31 16:58:21 -0300

    Update 032.py

--- bruno/Senai/032.py
+++ bruno/Senai/032.py
@@ -1,10 +1,7 @@
 # Escreva um programa que imprima todos os números pares entre dois números fornecidos pelo usuário.
 
-
-
-n1 = int(input('Um numero:? '))
+n1 = int(input('Um numero: '))
 n2 = int(input('um outro numero: '))
-
 
 for i in range(n1, n2 + 1):
     if i % 2 == 0:
